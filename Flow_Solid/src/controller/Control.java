1631628112a995234b842156420db63e383d33a9
Armandi <Jonas_hjp@hotmail.com>
2014-11-18 15:14:10 +0100

    THE 100% Test done

--- Flow_Solid/src/controller/Control.java
+++ Flow_Solid/src/controller/Control.java
@@ -9,6 +9,7 @@ package controller;
 import interfaces.WordPairControlInterface;
 import model.WordPairs;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -23,6 +24,7 @@ public class Control implements WordPairControlInterface {
      private int currentQuestion;
      
      Random rn = new Random();
+     FileHandler fh = new FileHandler();
      
     /**
      * Pre: Post: A new word pair is added to the existing collection of word
@@ -81,8 +83,12 @@ public class Control implements WordPairControlInterface {
      */
     @Override
     public String lookup(String question) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    
+        for (WordPairs wordPairs : words) {
+            if( wordPairs.getDanish().equals(question) ){
+                return wordPairs.getPolish();
+            }
+        }
+        return null;
     }
 
     /**
@@ -92,19 +98,7 @@ public class Control implements WordPairControlInterface {
      */
     @Override
     public boolean load(String filename) {
-    
-        try {
-            File file = new File(filename);
-            Scanner scan = new Scanner(file);
-            while(scan.hasNext()) {
-                words.add(new WordPairs((scan.next().replace(",", "")),scan.next()));
-            }
-        System.out.println(words);
-        return true;
-        
-        }catch (Exception e) {
-            return false;
-        }
+          return fh.readFile(filename, words);
     }
 
     /**
@@ -113,7 +107,9 @@ public class Control implements WordPairControlInterface {
      */
     @Override
     public boolean save(String filename) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    
+        return fh.writeToFile(filename, words);
     }
 
     /**
--- Flow_Solid/src/controller/FileHandler.java
+++ Flow_Solid/src/controller/FileHandler.java
@@ -12,20 +12,22 @@ import model.WordPairs;
  * @author Armandi
  */
 public class FileHandler {
-    //Firstly we will make an ArrayList for the text file.
-    ArrayList<WordPairs> words = new ArrayList();
+   
+  
     
     /*
     We will make a method which can read the text file.
     */
     
-    public ArrayList<WordPairs> readFile(){        
+    public boolean readFile(String filename, ArrayList<WordPairs> listWords){        
         //We'll make a scanner to the text file. We set the scanner to read from file source.
         Scanner scan = null;
         try {
-        scan = new Scanner(new File("Word.txt"));  
+        scan = new Scanner(new File(filename));  
         } catch (Exception e) {
+            
             e.printStackTrace();
+            return false;
         }
         while(scan.hasNext()){
             String str = scan.nextLine();
@@ -36,31 +38,31 @@ public class FileHandler {
             //Parsing the data to int's. Then adding them to the ArrayList created in the beginning.
             String danish = tokens[0].trim();
             String polish = tokens[1].trim();
-            
-            WordPairs w = new WordPairs(danish, polish);
-            //Now add the data to the ArrayList we created in the beginning.
-            words.add(w);
+            WordPairs wp = new WordPairs(danish, polish);
+            listWords.add( wp  );
         }
         
-        return words;
+        return true;
     }
     
     /*
     method for overwriting the textfile.
     */
-    public void writeToFile(){
+    public boolean writeToFile(String filename, ArrayList<WordPairs> listWords){
     
     FileWriter fw;
         try {
-    fw = new FileWriter("Word.txt");
-            for (WordPairs w : words) {
+    fw = new FileWriter(filename);
+            for (WordPairs w : listWords) {
                 fw.append(w.getDanish()+","+w.getPolish());
                 fw.append("\n");
             }
             fw.close();
         } catch (Exception e) {
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
 }
     
